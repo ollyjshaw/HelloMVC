295f6ff36b8b2758b5d8618ed35547b24e484b3c
Oliver Shaw <ollyjshaw@gmail.com>
2015-11-29 14:03:18 +0000

    initial commit

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNet.Mvc;
+using MvcSample.Web.Models;
+
+namespace MvcSample.Web
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View(CreateUser());
+        }
+
+        public User CreateUser()
+        {
+            User user = new User()
+            {
+                Name = "Dr BURA",
+                Address = "Daaan Saaarff"
+            };
+
+            return user;
+        }
+    }
+}
